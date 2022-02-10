a9d5b39bc0aefccb4b9b940d59bce19b1f7c2888
Chante Blais <chante.blais@kleientertainment.com>
2022-02-09 20:24:10 -0800

    working

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 class App {
     initProject() {
         let background = document.getElementById("background");
+        let audio = new Audio('Audio/searching.wav')
+
+        document.getElementById("background").addEventListener('mousedown', function () {
+            let playPromise = audio.play();
+            if (playPromise !== undefined) {
+                playPromise.then(function() {
+                }).catch(function(error) {
+                    console.log(error)
+                });
+            }
+        })
 
         let x = 0;
         let y = 0;
-        let scrollSpeed = 4;
+        let scrollSpeed = 6;
 
         document.querySelector('.left').addEventListener('mouseover', function () {
             this.iid = setInterval(function() {
@@ -20,7 +31,7 @@ class App {
 
         document.querySelector('.right').addEventListener('mouseover', function () {
             this.iid = setInterval(function() {
-                if (x > -Math.abs(background.offsetWidth / 2)) {
+                if (x > -Math.abs(background.offsetWidth / 2 ) +200) {
                     x -= scrollSpeed
                 }
                 background.style.left = x;
@@ -29,10 +40,26 @@ class App {
         document.querySelector('.right').addEventListener('mouseleave', function () {
             this.iid && clearInterval(this.iid);
         });
+
+        document.querySelector('.item').addEventListener('mousedown', function() {
+            this.remove()
+            addInventory(this.getAttribute('title'));
+        })
     }
 }
 
 window.onload = function () {
     window.app = new App();
     window.app.initProject()
+}
+
+function addInventory(name) {
+    let inventory = document.querySelector('.inventoryItem');
+    let newInventory = inventory.cloneNode(true);
+    let url = "url('Art/click_$.png')"
+    url = url.replace('$', name);
+    newInventory.style.background = url;
+    newInventory.style.backgroundRepeat = 'no-repeat';
+    document.getElementById("inventory").appendChild(newInventory)
+    newInventory.style.visibility = 'visible';
 }
